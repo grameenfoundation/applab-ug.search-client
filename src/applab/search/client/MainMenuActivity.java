e5e81eae2fb7cc387db143e7bffb1f67337d6125
kwolf <kwolf@8df75f15-59d7-5c4e-8359-c3bff8332439>
2010-09-21 03:17:27 +0000

    Unused imports
    git-svn-id: http://pmo.applab.org:81/svn/ckw/products/search.client@418 8df75f15-59d7-5c4e-8359-c3bff8332439

--- src/applab/search/client/MainMenuActivity.java
+++ src/applab/search/client/MainMenuActivity.java
@@ -14,7 +14,6 @@ package applab.search.client;
 
 import android.content.Intent;
 import android.text.InputFilter;
-import android.text.Spanned;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
